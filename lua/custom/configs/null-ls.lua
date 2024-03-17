9f607041afa2a9311f75b8d1e46f2096d3a64590
Kaustubh Kulkarni <kmkapril15@gmail.com>
2024-03-17 01:09:27 -0600

    null-ls: uncomment python null-ls

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -3,16 +3,16 @@ local null_ls = require("null-ls")
 
 local opts = {
     sources = {
-        -- null_ls.builtins.formatting.black.with({
-        --     extra_args = { "--line-length", "79" }
-        -- }),
-        -- null_ls.builtins.diagnostics.ruff,
-        -- null_ls.builtins.diagnostics.mypy.with({
-        --     extra_args = function()
-        --         local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-        --         return { "--ignore-missing-imports", "--python-executable", virtual .. "/bin/python3" }
-        --     end,
-        -- }),
+        null_ls.builtins.formatting.black.with({
+            extra_args = { "--line-length", "79" }
+        }),
+        null_ls.builtins.diagnostics.ruff,
+        null_ls.builtins.diagnostics.mypy.with({
+            extra_args = function()
+                local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+                return { "--ignore-missing-imports", "--python-executable", virtual .. "/bin/python3" }
+            end,
+        }),
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.golines,
