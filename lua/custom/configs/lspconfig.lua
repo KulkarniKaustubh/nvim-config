8e159b2f06b12046370c025cbce2a29c2c5b6a19
Kaustubh Kulkarni <kmkapril15@gmail.com>
2024-03-17 01:08:55 -0600

    too many changes

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,9 +4,29 @@ local on_attach = config.on_attach
 local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
+-- local util = require "lspconfig/util"
 
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {"python"},
-})
+lspconfig.pyright.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"python"},
+}
+
+lspconfig.gopls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    dap_debug = true,
+    dap_debug_gui = true,
+    cmd = {"gopls"},
+    filetypes = {"go", "gomod", "gowork", "gotmpl"},
+    -- root_dir = util.root_pattern("go.work", "go.mod", ".git")
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            }
+        },
+    },
+}
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -3,7 +3,9 @@ vim.opt.colorcolumn = "80"
 vim.opt.relativenumber = true
 vim.opt.guifont = "monospace:h11"
 vim.opt.scrolloff = 10
-vim.opt.tabstop = 4                             -- insert 4 spaces for a tab
+vim.opt.tabstop = 4                    -- insert 4 spaces for a tab
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Setting the invisible characters to show
 vim.opt.listchars = {
@@ -19,7 +21,7 @@ vim.opt.listchars = {
 --- Python stuff
 vim.g.loaded_python3_provider = nil
 
-local venv = "~/Envs/lsp"  -- Path to the environment to use with LSP
+local venv = "~/Envs/lspenv"  -- Path to the environment to use with LSP
 vim.g.python3_host_prog = vim.fn.expand(venv .. "/bin/python")
 vim.env.VIRTUAL_ENV = vim.fn.expand(venv)
 vim.env.PATH = vim.fn.expand(venv) .. "/bin" .. ":" .. vim.env.PATH
